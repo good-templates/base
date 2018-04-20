4d0d3c3b81d85f74b2f2ead17f9eb54082ba3e8c
good-templates <446100020@qq.com>
2018-04-20 14:59:39 +0800

    init

---
+++ template/src/js/main.js
@@ -0,0 +1,22 @@
+require('../index.html')
+require('../css/package.scss')
+var Zepto = require('npm-zepto')
+
+/**
+ * init
+ */
+function init() {
+  console.log('init ok')
+}
+/**
+ * start
+ */
+Zepto(function($){
+  setTimeout(function(){
+  	$('#good_loading').remove()
+    $('#good_main').removeClass('hide')
+
+    init()
+  },1000)
+  
+})
